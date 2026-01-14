6f838f921eaf772e948237f17527a311d5a95384
KillariDev <killari@gmail.com>
2026-01-14 17:34:35 +0200

    move factories to separate folder

--- solidity/contracts/peripherals/factories/AuctionFactory.sol
+++ solidity/contracts/peripherals/factories/AuctionFactory.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: UNICENSE
 pragma solidity 0.8.33;
-import { Auction } from './Auction.sol';
+import { Auction } from '../Auction.sol';
 
 contract AuctionFactory {
 	function deployAuction(bytes32 salt) external returns (Auction) {
