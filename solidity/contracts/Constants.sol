443ac93d43301bfe585456e716dad9f7fff95f20
KillariDev <killari@gmail.com>
2026-02-13 14:37:28 +0200

    accounted in previous prs

--- solidity/contracts/Constants.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: Unlicense
-pragma solidity 0.8.33;
-
-library Constants {
-	address constant GENESIS_REPUTATION_TOKEN = 0x221657776846890989a759BA2973e427DfF5C9bB;
-	uint48 constant YEAR_2099 = 4080321626;
-	uint256 constant NUM_OUTCOMES = 3;
-	address constant BURN_ADDRESS = 0xDeaDbeefdEAdbeefdEadbEEFdeadbeEFdEaDbeeF;
-}
