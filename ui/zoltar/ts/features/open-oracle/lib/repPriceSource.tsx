b9f867c36d7513e51d5c3fb14565351db366476c
KillariDev <13102010+KillariDev@users.noreply.github.com>
2026-08-17 20:29:09 +0000

    Point repPriceSource at coreShared pricing copy

--- ui/zoltar/ts/features/open-oracle/lib/repPriceSource.tsx
+++ ui/zoltar/ts/features/open-oracle/lib/repPriceSource.tsx
@@ -1,5 +1,5 @@
 import * as commonCopy from '@zoltar/ui-core-shared/copy/common.js'
-import * as pricingCopy from '../../../copy/pricing.js'
+import * as pricingCopy from '@zoltar/ui-core-shared/copy/pricing.js'
 import { assertNever } from '@zoltar/ui-core-shared/lib/assert.js'
 
 export type RepPriceSource = 'v4' | 'v3' | 'mock'
