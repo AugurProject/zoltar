adcb51f06c52fea033f6ac4e5d50a828424bf52c
KillariDev <13102010+KillariDev@users.noreply.github.com>
2026-08-17 20:27:48 +0000

    Install zoltar routing config at bootstrap and mount the zoltar App

--- ui/zoltar/ts/index.ts
+++ ui/zoltar/ts/index.ts
@@ -1,3 +1,15 @@
+import { createElement } from 'preact'
 import { mountApp } from '@zoltar/ui-core-shared/app/appRoot.js'
+import { installRouting } from '@zoltar/ui-core-shared/lib/routing.js'
+import { App } from './app/App.js'
 
-void mountApp()
+installRouting({
+	defaultRoute: 'zoltar',
+	routes: [
+		{ hash: '#/deploy', name: 'deploy' },
+		{ hash: '#/zoltar', name: 'zoltar', queryParameters: new Set(['universe', 'zoltarView']) },
+		{ hash: '#/open-oracle', name: 'open-oracle', queryParameters: new Set(['openOracleView', 'openOracleReportId']) },
+	],
+})
+
+void mountApp({ root: () => createElement(App, {}) })
