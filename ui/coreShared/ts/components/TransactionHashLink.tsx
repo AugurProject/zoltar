6b2a17db44f124e2eb441e2e56afce15af36a56c
KillariDev <13102010+KillariDev@users.noreply.github.com>
2026-09-25 08:41:49 +0000

    Show transaction hashes as unboxed explorer links

--- ui/coreShared/ts/components/TransactionHashLink.tsx
+++ ui/coreShared/ts/components/TransactionHashLink.tsx
@@ -12,7 +12,7 @@ export function TransactionHashLink({ hash }: TransactionHashLinkProps) {
 	if (transactionUrl === undefined) return <span className='transaction-hash-link'>{hash}</span>
 
 	return (
-		<a className='button-link secondary-link transaction-hash-link' href={transactionUrl} target='_blank' rel='noreferrer' aria-label={transactionCopy.formatViewTransactionOnExplorer(hash)}>
+		<a className='transaction-hash-link' href={transactionUrl} target='_blank' rel='noreferrer' aria-label={transactionCopy.formatViewTransactionOnExplorer(hash)}>
 			<span>{hash}</span>
 		</a>
 	)
